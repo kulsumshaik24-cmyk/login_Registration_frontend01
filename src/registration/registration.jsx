7e9461f596f54812046590f1fdaaa55fedbac6ac
kulsum shaik <kulsum.shaik24@sasi.ac.in>
2026-07-13 23:13:13 +0530

    Update registration.jsx

--- src/registration/registration.jsx
+++ src/registration/registration.jsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import './registration.css';
 
-const API_BASE_URL = 'http://localhost:5000';
+const API_BASE_URL = 'https://login-registration-backend01.onrender.com';
 
 const Register = () => {
   const navigate = useNavigate();
@@ -215,3 +215,5 @@ const Register = () => {
 };
 
 export default Register;
+
+
