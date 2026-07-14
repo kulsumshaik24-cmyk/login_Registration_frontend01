ebd9b86add6edd5f63b3951b65e6e2fcdd7ccc90
kulsum shaik <kulsum.shaik24@sasi.ac.in>
2026-07-14 18:13:54 +0530

    Update dashboard.jsx

--- src/dashboard/dashboard.jsx
+++ src/dashboard/dashboard.jsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import './dashboard.css';
 
-const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || 'http://localhost:5000';
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || 'https://login-registration-backend01.onrender.com';
 
 const Dashboard = () => {
   const navigate = useNavigate();
